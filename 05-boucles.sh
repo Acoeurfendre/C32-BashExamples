54db2b72831beb8cf96dab520a72e08ea31e7c7c
Marie-Soleil Carrier <mariecarrier@outlook.com>
2023-05-01 10:43:54 -0400

    For, foreach, while

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+    echo $i
+done
+
+# `` exécute une commande et met son résultat dans une variable
+liste=`ls`
+
+for fichier in $liste
+do 
+    echo $fichier
+done
+
+# les guillemets sont facultatifs autour de la lettre.
+resultat=o
+
+while [[ $resultat = o ]]
+do 
+    read -p "Choix : " resultat
+done 
