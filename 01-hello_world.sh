62eb26ee3c638d0b4d73d8a7ee57c4e62d3f6ee5
Marie-Soleil Carrier <mariecarrier@outlook.com>
2023-05-01 09:46:13 -0400

    Ajout para echo, read & fonctionnement variables

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+# permet d'afficher à l'écran
 echo "Hello" $USER
+
+# exit permet de quitter un programme
+
+# -n permet de ne pas faire de saut de ligne
+# -e permet d'interpréter les retours de ligne
+
+# read permet de prendre une valeur et le -p permet de spécifier 
+# quand on MET dans un variable, pas de $
+# quand on veut la VALEUR d'une variable, on met $. 
+read -p "Quel âge avez-vous ?" age
+echo $age "! Vous êtes jeune."
