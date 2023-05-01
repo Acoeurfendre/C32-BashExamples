7e9b6c8755e7d638a58d534ae7225e0bd415228b
Marie-Soleil Carrier <mariecarrier@outlook.com>
2023-05-01 12:19:20 -0400

    Exercice 3

--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,45 @@
 #!/bin/bash
+
+# MA VERSION 
+afficherEspaces() {
+        echo -n " "  
+}
+for ((i=0;i<30;i++))
+do
+clear 
+    for((j=0;j<i;j++))
+    do
+        afficherEspaces
+    done
+    if [[ $i -eq 29 ]]
+    then
+        echo -n "BOOM"
+    else
+        echo -n "@"
+    fi
+    sleep 0.5
+done
+
+# LA VERSION DU PROF
+boucles=30
+faireEspaces() {
+    for((i=0;i < $1;i++))
+    do
+        echo -n " "
+    done
+}
+
+for ((i=0;i<boucles;i++))
+do
+    clear
+    faireEspaces $i
+    if [[ $i -lt $((boucles)) ]]
+    then
+        echo -n "@"
+    else
+        echo -n "BOOM"
+    fi
+    sleep 0.5
+done
+
+
