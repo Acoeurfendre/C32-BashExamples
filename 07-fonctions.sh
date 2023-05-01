7f58ae35f237b7a2abbdcad32d2c92b6aaca01b7
Marie-Soleil Carrier <mariecarrier@outlook.com>
2023-05-01 10:56:04 -0400

    Fonctions et paramètres

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+# $0 donne le nom du script ou de la fonction.
+# $1-9 donne le paramètre respectif.
+maFonction() {
+    echo "Paramètre : " $1
+}
+
+# passer des paramèrtres à la fonction
+maFonction "salut"
+maFonction "toi"
