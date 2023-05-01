9d4e97d006615dc00964dbd66dbe8f1620391a89
Marie-Soleil Carrier <mariecarrier@outlook.com>
2023-05-01 11:44:31 -0400

    Exercice 2

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+afficherMenu() {
+    clear
+    echo "-------------------------"
+    echo " Logiciel................ "
+    echo "-------------------------"
+    echo "Quitter (q)"
+    echo "Dictionnaire (d)"
+}
+
+choix=
+while [[ $choix != q ]]
+do
+    afficherMenu
+    read -p "Choix : " choix
+    if [[ $choix = d ]]
+    then
+        ./exercice1.sh
+        sleep 1
+    fi
+done
