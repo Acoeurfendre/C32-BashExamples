76994102ba9b90052fa70b84d43c7637c852a796
Marie-Soleil Carrier <mariecarrier@outlook.com>
2023-05-01 10:03:40 -0400

    Méthodes (2) pour calculs mathématiques

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+# pour faire un calcul mathématique ou pour assigner le résultat du calcul dans une variable
+echo $((10+10))
+valeur=$((10+10))
+echo $valeur
+
+# autre manière de procéder (avec let)
+read nb1
+read nb2
+let resultat=$nb1*$nb2
+echo $resultat
